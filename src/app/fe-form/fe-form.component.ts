440e1be57a6042d4a729ff41475dfd9925e7d807
harkunovdmytro <harkundmytro@gmail.com>
2022-09-09 16:58:19 +0300

    fixes after review #2

--- src/app/fe-form/fe-form.component.ts
+++ src/app/fe-form/fe-form.component.ts
@@ -1,28 +1,22 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms'
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { SendFormService } from '../services/send-form.service';
 import { Validators } from '@angular/forms';
 import { FormRequest } from '../interfaces/form-request';
-
-const getFrameworksVersions = () => ({
-  angular: ['1.1.1', '1.2.1', '1.3.3'],
-  react: ['2.1.2', '3.2.4', '4.3.1'],
-  vue: ['3.3.1', '5.2.1', '5.1.3'],
-  '': []
-});
+import { frameworks, frameworksVersions } from '../constants';
 
 @Component({
   selector: 'app-fe-form',
   templateUrl: './fe-form.component.html',
-  styleUrls: ['./fe-form.component.scss']
+  styleUrls: ['./fe-form.component.scss'],
 })
 export class FeFormComponent {
-  frameworks = ['angular', 'react', 'vue'];
 
-  frameworksVersions: { [index: string]: any } = getFrameworksVersions();
-  
-  hobby = new FormArray([
-    this.createHobbyFormGroup()
+  frameworks: string[] = frameworks;
+  frameworksVersions: { [index: string]: string[] } = frameworksVersions;
+
+  hobbies = new FormArray([
+    this.createHobbyFormGroup(),
   ]);
 
   form = new FormGroup({
@@ -32,30 +26,34 @@ export class FeFormComponent {
     framework: new FormControl('', { validators: [Validators.required] }),
     frameworkVersion: new FormControl('', { validators: [Validators.required] }),
     email: new FormControl('', { validators: [Validators.required, Validators.email] }),
-    hobby: this.hobby
+    hobbies: this.hobbies,
   });
 
   get currentFramework(): string {
     return (<FormControl>this.form.get('framework')).value ?? '';
-  }
+  };
 
   get currentVersions(): string[] {
     return this.frameworksVersions[this.currentFramework];
-  }
+  };
 
   constructor(private sendFormService: SendFormService) { }
 
+  isFieldValid(fieldName: string): boolean {
+    return Boolean(this.form.get(fieldName)?.invalid && this.form.get(fieldName)?.touched);
+  };
+
   addHobby(event: Event): void {
     event.preventDefault();
-    this.hobby.push(this.createHobbyFormGroup());
+    this.hobbies.push(this.createHobbyFormGroup());
   };
 
   removeHobby(event: Event, id: number): void {
     event.preventDefault();
-    this.hobby.removeAt(id);
+    this.hobbies.removeAt(id);
   };
 
-  postForm() {
+  postForm(): void {
     const formDate = new Date(Date.parse((<FormControl>this.form.get('dateOfBirth'))?.value));
 
     const day = formDate.getDay() + 4;
@@ -65,19 +63,32 @@ export class FeFormComponent {
     const date = (day > 9 ? day : '0' + day) + '-'
       + (month > 9 ? month : '0' + month) + '-' + year;
 
-    const hobby = this.form.get('hobby')?.value.map((item) => ({ ...item, duration: (item.duration + ' month') }));
+    const hobbies = this.form.get('hobbies')?.value.map((item) => ({ ...item, duration: (item.duration + ' month') }));
 
     this.sendFormService.sendForm(<FormRequest>{
       ...this.form.value,
-      dateOfBirth: date,
-      hobby: hobby,
-    })
+      dateOfBirth: this.formatDate((<FormControl>this.form.get('dateOfBirth'))?.value),
+      hobbies,
+    });
   };
 
   private createHobbyFormGroup() {
     return new FormGroup({
       name: new FormControl('', { validators: [Validators.required] }),
       duration: new FormControl('', { validators: [Validators.required] })
-    }, { validators: [Validators.required] })
-  }
+    }, { validators: [Validators.required] });
+  };
+
+  private formatDate(strDate: string): string {
+    const formDate = new Date(Date.parse(strDate));
+
+    const day = formDate.getDay() + 4;
+    const month = formDate.getMonth() + 1;
+    const year = formDate.getFullYear();
+
+    const date = (day > 9 ? day : '0' + day) + '-'
+      + (month > 9 ? month : '0' + month) + '-' + year;
+
+    return date;
+  };
 };
--- src/app/interfaces/form-request.ts
+++ src/app/interfaces/form-request.ts
@@ -1,12 +1,16 @@
 export interface FormRequest {
     firstName: string;
     lastName: string;
-    "dateOfBirth": string,
-    "framework": string,
-    "frameworkVersion": string,
-    "email": string,
-    "hobby": {
-        "name": string,
-        "duration": string
-    }[]
+    dateOfBirth: string;
+    framework: string;
+    frameworkVersion: string;
+    email: string;
+<<<<<<< HEAD
+    hobbies: {
+=======
+    hobby: {
+>>>>>>> 11dcab86b028e4576c556045f9248d4ece2c738b
+        name: string;
+        duration: string;
+    }[];
 };
